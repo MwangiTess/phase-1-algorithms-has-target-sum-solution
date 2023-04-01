d0c31b734d54c9a0f55785fda34812f955a097a7
Teresia <teresiamwangi26@gmail.com>
2023-04-02 00:05:16 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // create an object to keep track of all the numbers we've seen
+  // 1 step
+  const seenNumbers = {};
+  // iterate over the array of numbers
+  for (const number of array) {
+    // n step
+    // for the current number, identify a complementary number that adds to our target
+    // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+    const complement = target - number;
+    // n step
+    // check if any of the keys in our object is the complement to the current number
+    // if so, return true
+    if (seenNumbers[complement]) return true;
+    // save the current number as the key on our object so we can check it later against other numbers
+    // n step
+    seenNumbers[number] = true;
+  }
+  // if we reach the end of the array, return false
+  // 1 step
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  Runtime: O(3n + 2) steps, which can be simplified to O(n)
+  Space Complexity: O(n) 
 */
 
 /* 
   Add your pseudocode here
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
 */
 
 /*
@@ -29,6 +59,23 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
